8ebd0ae996a38f8d15f74edc7ca5426b73db5976
Diego Manildo <diegothiry26@gmail.com>
2025-01-23 12:31:55 -0300

    Created basic website

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gmail, setGmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const showData = () => {
+    alert(name + "\n" + lastName + "\n" + gmail + "\n" + password)
+  }
+
+  return (
+    <div className="App">
+      <div className="data">
+        <label>Name: <input onChange={e => setName(e.target.value)} type="text"></input></label>
+        <label>Last name: <input onChange={e => setLastName(e.target.value)} type="text"></input></label>
+        <label>Gmail: <input onChange={e => setGmail(e.target.value)} type="email"></input></label>
+        <label>Password: <input onChange={e => setPassword(e.target.value)} type="password"></input></label>
+        <button onClick={showData}>Register</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
